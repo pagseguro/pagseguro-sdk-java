71d469e159a78b6981349ae47b3e05ba67d83726
Carine Bockorny <cbockorny@wmixvideo.com.br>
2017-08-16 08:50:25 -0300

    Comentado método que criava o arquivo de log e removido arquivo properties.

--- source/src/main/java/br/com/uol/pagseguro/api/utils/logging/SimpleLoggerFactory.java
+++ source/src/main/java/br/com/uol/pagseguro/api/utils/logging/SimpleLoggerFactory.java
@@ -1,20 +1,20 @@
 /*
  * 2007-2016 [PagSeguro Internet Ltda.]
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * Copyright: 2007-2016 PagSeguro Internet Ltda.
  * Licence: http://www.apache.org/licenses/LICENSE-2.0
  */
@@ -47,7 +47,7 @@ public class SimpleLoggerFactory extends LoggerFactory {
    */
   private Logger getLog(String name) {
     final Logger logger = Logger.getLogger(name);
-    logger.addHandler(getFileHandler());
+//    logger.addHandler(getFileHandler());
     logger.setLevel(Level.ALL);
     return logger;
   }
@@ -57,19 +57,19 @@ public class SimpleLoggerFactory extends LoggerFactory {
    *
    * @return File Handler
    */
-  private FileHandler getFileHandler() {
-    try {
-      if (FILEHANDLER == null) {
-        FILEHANDLER = new FileHandler("./pagseguro.log", true);
-        FILEHANDLER.setFormatter(new PagSeguroFormatter());
-        FILEHANDLER.setEncoding("UTF-8");
-        FILEHANDLER.setLevel(Level.ALL);
-      }
-      return FILEHANDLER;
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
+//  private FileHandler getFileHandler() {
+//    try {
+//      if (FILEHANDLER == null) {
+//        FILEHANDLER = new FileHandler("./pagseguro.log", true);
+//        FILEHANDLER.setFormatter(new PagSeguroFormatter());
+//        FILEHANDLER.setEncoding("UTF-8");
+//        FILEHANDLER.setLevel(Level.ALL);
+//      }
+//      return FILEHANDLER;
+//    } catch (IOException e) {
+//      throw new RuntimeException(e);
+//    }
+//  }
 
   /**
    * Constructs the logger by name
