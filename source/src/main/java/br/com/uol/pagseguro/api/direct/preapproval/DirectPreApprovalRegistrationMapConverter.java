5abc4578c6ce2490880f7550c66ee582d81bba5a
gabriellucius <gabriel_lucius@outlook.com>
2018-05-19 12:06:17 -0300

    feature: add receiver param json converter to direct pre approval registration

--- source/src/main/java/br/com/uol/pagseguro/api/direct/preapproval/DirectPreApprovalRegistrationMapConverter.java
+++ source/src/main/java/br/com/uol/pagseguro/api/direct/preapproval/DirectPreApprovalRegistrationMapConverter.java
@@ -1,9 +1,6 @@
 package br.com.uol.pagseguro.api.direct.preapproval;
 
-import br.com.uol.pagseguro.api.common.domain.converter.ParameterV2MapConverter;
-import br.com.uol.pagseguro.api.common.domain.converter.PreApprovalJsonConverter;
-import br.com.uol.pagseguro.api.common.domain.converter.SenderV2MapConverter;
-import br.com.uol.pagseguro.api.common.domain.converter.ShippingV2MapConverter;
+import br.com.uol.pagseguro.api.common.domain.converter.*;
 import br.com.uol.pagseguro.api.utils.AbstractJsonConverter;
 import br.com.uol.pagseguro.api.utils.AbstractMapConverter;
 import br.com.uol.pagseguro.api.utils.RequestJson;
@@ -11,6 +8,7 @@ import br.com.uol.pagseguro.api.utils.RequestMap;
 //@TODO change for ...RegistrationJsonConverter
 public class DirectPreApprovalRegistrationMapConverter extends AbstractJsonConverter<DirectPreApprovalRegistration> {
     private final static PreApprovalJsonConverter PRE_APPROVAL_JC = new PreApprovalJsonConverter();
+    private final static ReceiverJsonConverter RECEIVER_JC = new ReceiverJsonConverter();
     private final static ParameterV2MapConverter PARAMETER_MC = new ParameterV2MapConverter();
 
     DirectPreApprovalRegistrationMapConverter() {
@@ -31,9 +29,10 @@ public class DirectPreApprovalRegistrationMapConverter extends AbstractJsonConve
         requestJson.putString("redirectURL", directPreApprovalRegistration.getRedirectURL());
         requestJson.putString("reviewURL", directPreApprovalRegistration.getReviewURL());
         requestJson.putJson(PRE_APPROVAL_JC.convert(directPreApprovalRegistration.getPreApproval()), "preApproval");
+        requestJson.putJson(RECEIVER_JC.convert(directPreApprovalRegistration.getReceiver()), "receiver");
 //        requestJson.jsonBody();
         //@TODO add parameter, same as pre approval objetc
 //        requestMap.putMap(PARAMETER_MC.convert(preApprovalRegistration.getParameters()));
-        //@TODO add receiver, same as pre approval objetc
+
     }
 }
