9cc4eeef7598d3783195efaa829f5384789b2dc0
EduardoSorokin <eduardosorokin@gmail.com>
2018-06-04 12:59:30 -0300

    add dependencies direct pre approval cancellation example

--- public/example-api/src/main/java/br/com/uol/pagseguro/example/api/direct/preapproval/CancelDirectPreApproval.java
+++ public/example-api/src/main/java/br/com/uol/pagseguro/example/api/direct/preapproval/CancelDirectPreApproval.java
@@ -3,6 +3,7 @@ package br.com.uol.pagseguro.example.api.direct.preapproval;
 import br.com.uol.pagseguro.api.PagSeguro;
 import br.com.uol.pagseguro.api.PagSeguroEnv;
 import br.com.uol.pagseguro.api.credential.Credential;
+import br.com.uol.pagseguro.api.direct.preapproval.DirectPreApprovalRequestCancellationBuilder;
 import br.com.uol.pagseguro.api.http.JSEHttpClient;
 import br.com.uol.pagseguro.api.utils.logging.SimpleLoggerFactory;
 
